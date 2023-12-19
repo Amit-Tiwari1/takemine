60b279432c89c96ade860ecb8f3c18daa6afe4ca
Amit Tiwari <aktsln28@gmail.com>
2023-12-20 00:02:01 +0530

    d

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
 import express from "express";
 import sendMail from "./src/controllers/sendemail.js";
-import { connection } from "./src/db/dbConnection.js";
-
-// Call the connection function
-connection();
-
-
-
+import { connection,result } from "./src/db/dbConnection.js";
+console.log("indfile",result)
 const PORT = 8000;
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Server is live");
 });
-
 app.get("/mail", sendMail);
+app.get("/getdata",(req,res)=>{
+  res.send("data is here",result)
+})
 
 const start_Server = async () => {
   try {
-  
-  
-
+    connection();
     app.listen(PORT || 5000, () => {
       console.log(`App is listening at ${PORT}`);
     });
