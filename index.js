3c54a76ce7a08064d38c8b89fb4941f37f6793e9
Amit Tiwari <70047168+Amit-Tiwari1@users.noreply.github.com>
2023-12-22 10:30:20 +0530

    dad

--- index.js
+++ index.js
@@ -15,9 +15,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use("/api", userroute);
-app.use("/api/verify", otpVerificationRoute); // Use the OTP verification route
-
-// ... (other routes and error handling)
+app.use("/api/verify", otpVerificationRoute);
 
 const start_Server = async () => {
   try {
